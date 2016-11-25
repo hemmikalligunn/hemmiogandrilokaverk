e4715e0c47939cc7ae71dba9fbd94a1e4dddfaea
hemmikalligunn <hemmikalligunn>
2016-11-25 14:41:04 +0000

    comment

--- Lokaverk/Lokaverk/Form1.cs
+++ Lokaverk/Lokaverk/Form1.cs
@@ -8,6 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+
 namespace Lokaverk
 {
     public partial class Form1 : Form
@@ -15,7 +16,7 @@ namespace Lokaverk
         int teljari1 = 0, teljari2 = 0;
         int currentX = 0;
         int currentY = 0;
-
+        //sdfjklkasdfsdajflsd
 
         Button[] World = new Button[1984];
 
@@ -100,6 +101,22 @@ namespace Lokaverk
 
             bits.Text = tempButton.Name;
         
+        }
+
+        private void textBox1_KeyDown(object sender, System.Windows.Forms.KeyEventArgs e)
+        {
+            // Determine whether the key entered is the F1 key. If it is, display Help.
+            if (e.KeyCode == Keys.F1 && (e.Alt || e.Control || e.Shift))
+            {
+                // Display a pop-up Help topic to assist the user.
+                Help.ShowPopup(textBox1, "Enter your name.", new Point(textBox1.Bottom, textBox1.Right));
+            }
+            else if (e.KeyCode == Keys.F2 && e.Modifiers == Keys.Alt)
+            {
+                // Display a pop-up Help topic to provide additional assistance to the user.
+                Help.ShowPopup(textBox1, "Enter your first name followed by your last name. Middle name is optional.",
+                    new Point(textBox1.Top, this.textBox1.Left));
+            }
         }
         //MapDesignTools
 
